19a1b39da4eaa9866388545b0af9374779b74da3
Sebastián <Sebastin@PC-SEBA>
2021-04-21 01:06:19 -0400

    Agregar archivos de proyecto.

---
+++ MainConsol.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certamen.NET
+{
+    class Consola
+    {
+        //Definir las variables
+        public String nombre;
+        public String apellido;
+        public double monto_solicitado;
+        public int cuotas;
+        public double monto_total;
+        public double valor_de_cuota;
+        public double interes;
+
+        //Crear método con public void (por ahora)
+        public void calcular_valor_cuotas()
+        {
+            if (monto_solicitado >= 500000)
+                if(cuotas >= 1 && cuotas <= 36)
+                {
+                    if (cuotas <= 12)
+                    {
+                        interes = monto_solicitado * 0.1;
+                        monto_total = monto_solicitado + interes;
+                        valor_de_cuota = monto_total / cuotas;
+                        Console.WriteLine("El valor de cuota es: "+valor_de_cuota);
+                    }else if(cuotas <= 24)
+                    {
+                        interes = monto_solicitado * 0.2;
+                        monto_total = monto_solicitado + interes;
+                        valor_de_cuota = monto_total / cuotas;
+                        Console.WriteLine("El valor de la cuota es: " + valor_de_cuota);
+                    }
+                    else if(cuotas <= 36)
+                    {
+                        interes = monto_solicitado * 0.35;
+                        monto_total = monto_solicitado + interes;
+                        valor_de_cuota = monto_total / cuotas;
+                        Console.WriteLine("El valor de la cuota es: " + valor_de_cuota);
+                    }
+
+                 }
+         }
+
+        public void calcular_monto_total()
+        {
+            monto_total = valor_de_cuota * cuotas;
+            Console.WriteLine("El monto total es: " + monto_total);
+        }
+        public void calcular_todo()
+        {
+            this.calcular_valor_cuotas();
+            this.calcular_monto_total();
+        }
+
+    }
+}
---
+++ Procedimientos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certamen.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Ponga el monto que solicitará :");
+                double monto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ponga una cantidad de cuotas :");
+            int cuotas = int.Parse(Console.ReadLine());
+
+            Consola c1 = new Consola();
+                c1.monto_solicitado = monto;
+                c1.cuotas = cuotas;
+
+            c1.calcular_todo();
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
